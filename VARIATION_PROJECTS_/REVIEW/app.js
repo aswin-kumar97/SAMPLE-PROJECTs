8f45f07e77c5af63b91d4ccf57b5972749a684b3
Aswin Deo <56706196+aswin-kumar97@users.noreply.github.com>
2021-07-20 22:44:28 +0530

    20-7-21

---
+++ VARIATION_PROJECTS_/REVIEW/app.js
@@ -0,0 +1,97 @@
+var reviews = [
+  {
+    id: 1,
+    name: "bathri",
+    job: "UX designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "If you set the textContent property, any child nodes are removed and replaced by a single Text node containing the specified string.",
+  },
+  {
+    id: 2,
+    name: "john",
+    job: "Team Leader",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Sometimes this property can be used instead of the nodeValue property, but remember that this property returns the text of all child nodes as well.",
+  },
+  {
+    id: 3,
+    name: "peter",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sometimes this property can be used instead of the nodeValue property, but remember that this property returns the text of all child nodes as well.",
+  },
+  {
+    id: 4,
+    name: "aswin",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "If you set the textContent property, any child nodes are removed and replaced by a single Text node containing the specified string. ",
+  },
+];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+randomBtn.addEventListener("click", function () {
+  console.log("hello");
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
+
+(function() {
+  showSlides();
+})();
+
+function showSlides() {
+  document.querySelector('.next-btn').click();
+  setTimeout(showSlides, 5000);
+}
+
