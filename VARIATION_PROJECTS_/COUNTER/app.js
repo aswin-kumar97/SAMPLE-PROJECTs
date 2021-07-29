d11ad8e9adeeb41ff19c87939458d94f7b55279c
Aswin Deo <56706196+aswin-kumar97@users.noreply.github.com>
2021-07-29 15:50:29 +0530

    variation

--- VARIATION_PROJECTS_/COUNTER/app.js
+++ VARIATION_PROJECTS_/COUNTER/app.js
@@ -2,6 +2,8 @@
 let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+var decr = document.querySelector(".decrease");
+var ddt = document.querySelector(".reset");
 
 
 btns.forEach(function (btn) {
@@ -16,24 +18,31 @@ btns.forEach(function (btn) {
     }
     if (count < 0) {
       decrease.disabled = true;
+
     }
     if (count > 0) {
       value.style.color = "green";
+      decr.classList.remove("disabled");
+      ddt.classList.remove("disabled");
+      decr.disabled = false;
     }
     if (count === 0) {
       value.style.color = "#222";
+      decr.classList.add("disabled");
+      ddt.classList.add("disabled");
+      decr.disabled = true;
 
     }
     value.textContent = count;
   });
 });
 
-function increaseBtn() {
-  var decr = document.querySelector(".decrease");
-  var ddt = document.querySelector(".reset");
-  decr.classList.remove("disabled");
-  ddt.classList.remove("disabled");
-};
+// function increaseBtn() {
+//   var decr = document.querySelector(".decrease");
+//   var ddt = document.querySelector(".reset");
+//   decr.classList.remove("disabled");
+//   ddt.classList.remove("disabled");
+// };
 
 
 
