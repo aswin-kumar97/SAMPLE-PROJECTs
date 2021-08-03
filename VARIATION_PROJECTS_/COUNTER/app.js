fc1460fe786cc65ffe14c56cecdca4ebaa504d58
Aswin Deo <56706196+aswin-kumar97@users.noreply.github.com>
2021-08-03 18:54:57 +0530

    03-8-21

--- VARIATION_PROJECTS_/COUNTER/app.js
+++ VARIATION_PROJECTS_/COUNTER/app.js
@@ -1,48 +1,10 @@
+class students {
+  constructor(email, name){
+    this.email = email;
+    this.name = name;
+  }
+}
 
-let count = 0;
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
-var decr = document.querySelector(".decrease");
-var ddt = document.querySelector(".reset");
+var userone = new students ('ak', 'aksks');
 
-
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
-    if (count < 0) {
-      decrease.disabled = true;
-    }
-    if (count > 0) {
-      value.style.color = "green";
-      decr.classList.remove("disabled");
-      ddt.classList.remove("disabled");
-      decr.disabled = false;
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-      decr.classList.add("disabled");
-      ddt.classList.add("disabled");
-      decr.disabled = true;
-
-    }
-    value.textContent = count;
-  });
-});
-
-// function increaseBtn() {
-//   var decr = document.querySelector(".decrease");
-//   var ddt = document.querySelector(".reset");
-//   decr.classList.remove("disabled");
-//   ddt.classList.remove("disabled");
-// };
-
-
-
-
+console.log(userone);
